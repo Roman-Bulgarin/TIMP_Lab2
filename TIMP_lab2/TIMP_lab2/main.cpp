d1795a7e8353772dc6706634e8fbe70ad8820a71
Roman-Bulgarin <147330001+Roman-Bulgarin@users.noreply.github.com>
2024-03-12 01:23:49 +0300

    main.cpp

--- TIMP_lab2/TIMP_lab2/main.cpp
+++ TIMP_lab2/TIMP_lab2/main.cpp
@@ -1,54 +1,36 @@
 #include <iostream>
 #include <cctype>
 #include "modAlphaCipher.h"
-
+#include <locale>
 using namespace std;
+
+void check(const wstring& Text, const wstring& key, const bool destructCipherText=false)
+{
+    try {
+        wstring cipherText;
+        wstring decryptedText;
+        modAlphaCipher cipher(key);
+        cipherText = cipher.encrypt(Text);
+        if (destructCipherText)
+            cipherText.front() = towlower(cipherText.front());
+        decryptedText = cipher.decrypt(cipherText);
+        wcout<<"key:"<<key<<endl;
+        wcout<<Text<<endl;
+        wcout<<cipherText<<endl;
+        wcout<<decryptedText<<endl;
+    } catch (const cipher_error & e) {
+        wcerr<<"Error: "<<e.what()<<endl;
+    }
+}
 
 int main()
 {
-    try
-    {
-        locale loc("ru_RU.UTF-8");
-        locale::global(loc);
-        wstring key;
-        wstring text;
-        unsigned op;
-
-        wcout << "Cipher ready. Input key: ";
-        wcin >> key;
-        wcout << "Key loaded\n";
-        modAlphaCipher cipher(key);
-
-        do
-        {
-            wcout << "Cipher ready. Input operation (0-exit, 1-encrypt, 2-decrypt): ";
-            wcin >> op;
-
-            if (op > 2)
-            {
-                wcout << "Illegal operation\n";
-            }
-            else if (op > 0)
-            {
-                wcout << "Cipher ready. Input text: ";
-                wcin.ignore();
-                getline(wcin, text);
-
-                if (op == 1)
-                {
-                    wcout << "Encrypted text: " << cipher.encrypt(text) << endl;
-                }
-                else
-                {
-                    wcout << "Decrypted text: " << cipher.decrypt(text) << endl;
-                }
-            }
-        } while (op != 0);
-    }
-    catch (const cipher_error &e)
-    {
-        cerr << "Error: " << e.what() << endl;
-    }
-
+    locale loc("ru_RU.UTF-8");
+    locale::global(loc);
+    check(L"РОМАН",L"КЛЮЧ");
+    check(L"РОМАН",L"Ключ");
+    check(L"Р56ОМА23Н",L"КЛЮЧ");
+    check(L"РУСЛАНОВИЧ",L"");
+    check(L"БУЛГАРИН",L"2004");
     return 0;
 }
