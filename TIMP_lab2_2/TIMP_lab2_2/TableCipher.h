90940e722f324045626a0f1ad1229e08c2d53667
Roman-Bulgarin <147330001+Roman-Bulgarin@users.noreply.github.com>
2024-03-12 01:32:18 +0300

    TableCipher.h

--- TIMP_lab2_2/TIMP_lab2_2/TableCipher.h
+++ TIMP_lab2_2/TIMP_lab2_2/TableCipher.h
@@ -1,40 +1,30 @@
 #pragma once
-#include <vector>
-#include <string>
-#include <map>
 #include <iostream>
+#include <string>
+#include <vector>
+#include <map>
 #include <cctype>
 #include <locale>
 #include <codecvt>
+#include <algorithm>
+#include <UnitTest++/UnitTest++.h>
+using namespace std;
 
-class cipher_error: public std::invalid_argument
-{
-public:
-    explicit cipher_error (const std::string& what_arg):
-        std::invalid_argument(what_arg) {}
-    explicit cipher_error (const char* what_arg):
-        std::invalid_argument(what_arg) {}
-};
-
-class tableCipher
+class modAlphaCipher
 {
 private:
-    int key1;
-    std::wstring numAlpha = L"АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЬЫЪЭЮЯ";
-    std::wstring lnumAlpha = L"абвгдеёжзийклмнопрстуфхцчшщьыъэюя";
-    bool is_rus(wchar_t wc);
-    int is_low_rus(wchar_t wch);
+    int newkey;
+    string getValidOpenText(const string & s);
 public:
-    
-    tableCipher()=delete;
-    tableCipher(const int& key) {
-        if (key<=0) {
-            throw cipher_error("Key is not valid\n");
-        } else {
-            key1=key;
-        }
-    }
-    std::wstring encrypt(std::wstring& open_text);
-    std::wstring decrypt(std::wstring& cipher_text);
-    std::wstring toValid(std::wstring& s);
+    modAlphaCipher()=delete;
+    modAlphaCipher(const int& key) :newkey(key) {}; 
+    string encrypt(const string& open_st);   
+    string decrypt(const string& cipher_st);
+};
+class cipher_error: public invalid_argument {
+public:
+	explicit cipher_error (const string& what_arg):
+		invalid_argument(what_arg) {}
+	explicit cipher_error (const char* what_arg):
+		invalid_argument(what_arg) {}
 };
