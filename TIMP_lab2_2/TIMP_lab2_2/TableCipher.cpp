3b56af8006e2976ec4d5e871138dd55b550ea66f
Roman-Bulgarin <147330001+Roman-Bulgarin@users.noreply.github.com>
2024-03-12 01:30:33 +0300

    TableCipher.cpp

--- TIMP_lab2_2/TIMP_lab2_2/TableCipher.cpp
+++ TIMP_lab2_2/TIMP_lab2_2/TableCipher.cpp
@@ -1,83 +1,57 @@
-#include  "TableCipher.h"
-#include <string>
-
-using namespace std;
-std::wstring tableCipher::toValid(std::wstring& s)
+#include "TableCipher.h"
+string modAlphaCipher::decrypt(const string& cipher_text) 
 {
-    using convert_type = std::codecvt_utf8<wchar_t>;
-    std::wstring_convert<convert_type, wchar_t> converter;
-
-    if (s.empty())
-        throw cipher_error("Empty text");
-
-    std::wstring tmp(s);
-    std::string st = converter.to_bytes(s);
-    for (auto & c:tmp) {
-        if (!is_rus(c))
-            throw cipher_error(("Text has invalid symbols: ") +st);
-        if (is_low_rus(c)!=-1)
-            c = numAlpha[is_low_rus(c)];
-    }
-    return tmp;
-}
-bool tableCipher::is_rus(wchar_t wch)
-{
-    std::wstring alf=lnumAlpha+numAlpha;
-    for (uint i=0; i<alf.length(); i++) {
-        if (wch == alf[i])
-            return true;
-    }
-    return false;
-}
-int tableCipher::is_low_rus(wchar_t wch)
-{
-    for (uint i=0; i<lnumAlpha.length(); i++) {
-        if (wch == lnumAlpha[i])
-            return i;
-    }
-    return -1;
-}
-std::wstring tableCipher::encrypt(std::wstring& open_text)
-{
-
-    wstring tabl = open_text;
-    int dl, nstrok, index, x;
-    while (tabl.size() % key1 != 0) {
-        tabl+=L"_";
-    }
-    open_text = tabl;
-    dl = tabl.size();
-    nstrok = (dl - 1) / key1 + 1;
-    x = 0;
-    wstring encrypted;
-    for (int i = key1; i > 0; i--) {
-        for (int j = 0; j < nstrok; j++) {
-            index = i+key1*j;
-            //wcout<<index<<endl;
-            if (index-1 < dl) {
-                tabl[x] = open_text[index-1];
-                x++;
+	string open_text = getValidOpenText(cipher_text);
+    string decrypted_text = open_text;
+    int text_length, position, line, i; 
+    text_length = cipher_text.size(); 
+    line = (text_length - 1) / newkey + 1; 
+    i = 0;
+    for (int column = newkey; column > 0; column--) { 
+        for (int row = 0; row < line; row++) { 
+            position = newkey * row + column; 
+            if (position - 1 < text_length) {
+                decrypted_text[position - 1] = cipher_text[i];
+                i++;
             }
         }
     }
-    return tabl;
+    return decrypted_text;
 }
-
-std::wstring tableCipher::decrypt(std::wstring& cipher_text)
+string modAlphaCipher::encrypt(const string& open_text) 
 {
-    wstring tabl = cipher_text;
-    int dl, nstrok, index, x;
-    dl = cipher_text.length();
-    nstrok = (dl - 1) / key1 + 1;
-    x = 0;
-    for (int i = key1; i > 0; i--) {
-        for (int j = 0; j < nstrok; j++) {
-            index = i+key1*j;
-            if (index-1 < dl) {
-                tabl[index-1] = cipher_text[x];
-                x++;
+	string valid_open_text = getValidOpenText(open_text);
+    string encrypted_text = valid_open_text;
+    int text_length, line, position, i; 
+    text_length = open_text.size(); 
+    line = (text_length - 1) / newkey + 1; 
+    i = 0;
+    for (int column = newkey; column > 0; column--) { 
+        for (int row = 0; row < line; row++) { 
+            position = column + newkey * row;
+            if (position - 1 < text_length) {
+                encrypted_text[i] = open_text[position - 1];
+                i++;
             }
         }
     }
-    return tabl;
+    return encrypted_text;
+}
+inline string modAlphaCipher::getValidOpenText(const string & text)
+{
+	if (text.empty()) {
+        throw cipher_error("Input text cannot be empty"); //Проверка на наличие текста
+    }
+    // Проверка длины ключа
+    if (static_cast<std::string::size_type>(newkey) > text.length()) {
+        throw cipher_error("Invalid key");
+    }
+    // Проверка на недопустимые символы
+    for (char c : text) {
+        if (!isalpha(c)) {
+            throw cipher_error("Invalid characters");
+        }
+    }
+    // Возвращаем длину строки, если все проверки пройдены
+    return text;
 }
